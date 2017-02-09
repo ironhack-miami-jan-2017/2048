adc815e74367e5bccb855ede7d9e2aa2898d6343
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-02-09 18:08:16 -0500

    Activate keyboard movements

--- js/app.js
+++ js/app.js
@@ -8,7 +8,46 @@ $(document).ready(function () {
 
   // 2. take the initial tiles and put them on the screen
   renderTiles();
+
+  // 3. handle keyboard events
+  $(document).keydown(moveGame);
 });
+
+
+function moveGame (ev) {
+  var acceptableKeys = [ 37, 65, 38, 87, 39, 68, 40, 83 ];
+
+  if (!acceptableKeys.includes(ev.keyCode)) {
+    return;
+  }
+
+  // prevent arrow key scrolling
+  ev.preventDefault();
+
+  // 4. move board in object based on keypresses (up, down, left, right)
+  // move if correct keys were pressed
+  switch (ev.keyCode) {
+    case 37:  // left arrow
+    case 65:  // a
+      myGlobalGame.move('left');
+      break;
+    case 38:  // up arrow
+    case 87:  // w
+      myGlobalGame.move('up');
+      break;
+    case 39:  // right arrow
+    case 68:  // d
+      myGlobalGame.move('right');
+      break;
+    case 40:  // down arrow
+    case 83:  // s
+      myGlobalGame.move('down');
+      break;
+  }
+
+  // 5. updating the screen based on new board state
+  renderTiles();
+}
 
 
 function renderTiles () {
@@ -28,7 +67,4 @@ function renderTiles () {
 }
 
 
-// 3. handle keyboard events
-// 4. move board in object based on keypresses (up, down, left, right)
-// 5. updating the screen based on new board state
 // 6. win or lose (maybe)
