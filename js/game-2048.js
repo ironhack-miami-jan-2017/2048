ff35a688706b82b905bf8480ba7c3691d40a8f16
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-02-09 12:12:41 -0500

    Add move left method

--- js/game-2048.js
+++ js/game-2048.js
@@ -59,3 +59,48 @@ Game2048.prototype._renderBoard = function () {
     console.log(row);
   });
 };
+
+
+Game2048.prototype.moveLeft = function () {
+  var updatedBoard = [];
+
+  this.board.forEach(function (row) {
+    // 1. Remove empties from row
+    var newRow = [];
+
+    row.forEach(function (cell) {
+      if (cell !== null) {
+        newRow.push(cell);
+      }
+    });
+
+    // 2. Merge tiles in row that are together and the same number
+    for (var i = 0; i < newRow.length; i += 1) {
+      if (newRow[i] === newRow[i + 1]) {
+        newRow[i] *= 2;
+        newRow[i + 1] = null;
+      }
+    }
+
+    // 3. Remove new empties in the middle
+    //     e.g. when step #2 turns [8, 8, 4] into [16, null, 4]
+    //          we want to end up with [16, 4]
+    var moved = [];
+
+    newRow.forEach(function (cell) {
+      if (cell !== null) {
+        moved.push(cell);
+      }
+    });
+
+
+    // 4. push() nulls until row has length 4 again
+    while (moved.length < 4) {
+      moved.push(null);
+    }
+
+    updatedBoard.push(moved);
+  });
+
+  this.board = updatedBoard;
+};
